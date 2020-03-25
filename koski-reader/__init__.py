f48e2529d8b4b466b2a66a1191ef68ae00e2b2d4
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-03-25 16:02:43 +0200

    Bump version: 0.2.1 → 0.3.0

--- koski-reader/__init__.py
+++ koski-reader/__init__.py
@@ -1,5 +1,5 @@
 """koski-reader - The rest clien which reading Koski database."""
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Pasi Ollikainen <pasi.ollikainen@outlook.com>'
 __all__ = []
